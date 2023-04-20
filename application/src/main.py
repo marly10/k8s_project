2652200a5ba0a2a0e5c48d88cdd2882f1cb1bbc9
Your Name <you@example.com>
2023-04-19 23:04:48 -0700

    Send it

--- application/src/main.py
+++ application/src/main.py
@@ -5,11 +5,13 @@ app = Flask(__name__)
 plants = []
 tasks = []
 
+
 class Plant:
     def __init__(self, name, type, age):
         self.name = name
         self.type = type
         self.age = age
+
 
 class Task:
     def __init__(self, title, description, done):
@@ -17,64 +19,89 @@ class Task:
         self.description = description
         self.done = done
 
+
 @app.route("/")
-def index():
-    return render_template('index.html', plants=plants, tasks=tasks)
+def root():
+    return render_template('home.html')
 
 
 @app.route("/plants", methods=["POST"])
 def create_plant():
-    data = request.get_json()
-    plant = Plant(data['name'], data['type'], data['age'])
+    name = request.form['name']
+    plant_type = request.form['type']
+    age = request.form['age']
+    plant = Plant(name, plant_type, age)
     plants.append(plant.__dict__)
-    return jsonify(plant.__dict__)
+    return render_template('plants.html', plants=plants)
+
 
 @app.route("/plants")
 def read_plants():
-    return jsonify(plants)
+    return render_template('plants.html', plants=plants)
+
 
 @app.route("/plants/<int:plant_id>")
 def read_plant(plant_id):
-    return jsonify(plants[plant_id])
+    return render_template('plant.html', plant=plants[plant_id])
 
-@app.route("/plants/<int:plant_id>", methods=["PUT"])
+
+@app.route("/plants/<int:plant_id>/edit", methods=["GET", "POST"])
 def update_plant(plant_id):
-    data = request.get_json()
-    plant = Plant(data['name'], data['type'], data['age'])
-    plants[plant_id] = plant.__dict__
-    return jsonify(plant.__dict__)
+    if request.method == "POST":
+        name = request.form['name']
+        plant_type = request.form['type']
+        age = request.form['age']
+        plant = Plant(name, plant_type, age)
+        plants[plant_id] = plant.__dict__
+        return render_template('plants.html', plants=plants)
+    else:
+        return render_template('edit_plant.html', plant=plants[plant_id])
 
-@app.route("/plants/<int:plant_id>", methods=["DELETE"])
+
+@app.route("/plants/<int:plant_id>/delete", methods=["POST"])
 def delete_plant(plant_id):
     plants.pop(plant_id)
-    return jsonify({"message": "Plant deleted"})
+    return render_template('plants.html', plants=plants)
+
 
 @app.route("/tasks", methods=["POST"])
 def create_task():
-    data = request.get_json()
-    task = Task(data['title'], data['description'], data['done'])
+    title = request.form['title']
+    description = request.form['description']
+    done = request.form['done']
+    task = Task(title, description, done)
     tasks.append(task.__dict__)
-    return jsonify(task.__dict__)
+    return render_template('tasks.html', tasks=tasks)
+
 
 @app.route("/tasks")
 def read_tasks():
-    return jsonify(tasks)
+    return render_template('tasks.html', tasks=tasks)
+
 
 @app.route("/tasks/<int:task_id>")
 def read_task(task_id):
-    return jsonify(tasks[task_id])
+    return render_template('task.html', task=tasks[task_id])
 
-@app.route("/tasks/<int:task_id>", methods=["PUT"])
+
+@app.route("/tasks/<int:task_id>/edit", methods=["GET", "POST"])
 def update_task(task_id):
-    data = request.get_json()
-    task = Task(data['title'], data['description'], data['done'])
-    tasks[task_id] = task.__dict__
-    return jsonify(task.__dict__)
+    if request.method == "POST":
+        title = request.form['title']
+        description = request.form['description']
+        done = request.form['done']
+        task = Task(title, description, done)
+        tasks[task_id] = task.__dict__
+        return render_template('tasks.html', tasks=tasks)
+    else:
+        return render_template('edit_task.html', task=tasks[task_id])
 
-@app.route("/tasks/<int:task_id>", methods=["DELETE"])
+
+@app.route("/tasks/<int:task_id>/delete", methods=["POST"])
 def delete_task(task_id):
     tasks.pop(task_id)
-    return jsonify({"message": "Task deleted"})
+    return render_template('tasks.html', tasks=tasks)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
